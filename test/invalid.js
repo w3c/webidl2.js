7fdfccbc881c3a44d98d0467727ca155a36e1b1a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-12-14 14:10:50 +0900

    use for-of on tests

--- test/invalid.js
+++ test/invalid.js
@@ -15,16 +15,17 @@ describe("Parses all of the invalid IDLs to check that they blow up correctly", 
   const idls = fs.readdirSync(dir)
     .filter(it => (/\.w?idl$/).test(it) && !skip[it])
     .map(it => pth.join(dir, it));
-  const errors = idls.map(it => pth.join(__dirname, "invalid", "json", pth.basename(it).replace(/\.w?idl/, ".json")));
 
-  for (let i = 0, n = idls.length; i < n; i++) {
-    const idl = idls[i];
-    const err = JSON.parse(fs.readFileSync(errors[i], "utf8"));
+  for (const idl of idls) {
+    const err = JSON.parse(fs.readFileSync(
+      pth.join(__dirname, "invalid", "json", pth.basename(idl).replace(/\.w?idl/, ".json")),
+      "utf8"
+    ));
 
     it(`should produce the right error for ${idl}`, () => {
       let error;
       try {
-        var ast = wp.parse(fs.readFileSync(idl, "utf8"));
+        const ast = wp.parse(fs.readFileSync(idl, "utf8"));
         console.log(JSON.stringify(ast, null, 4));
       }
       catch (e) {
