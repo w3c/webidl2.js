1e29dcb71bef957fef4b15690a0ac8ec3d3533d9
Kagami Sascha Rosylight <saschaplas@outlook.com>
2018-01-29 15:05:03 +0900

    Auto acquisition for parser result changes (#130)
    * add collect.js
    
    * auto diff acquisition
    
    * json changes (whitespaces/reorderings)
    
    * add proper jsdoc
    
    * class TestItem
    
    * use option bag
    
    * use collect() on writer test

--- test/invalid.js
+++ test/invalid.js
@@ -4,38 +4,17 @@
 
 "use strict";
 
-const wp = require("../lib/webidl2");
-const expect = require("expect");
-const pth = require("path");
+const { collect } = require("./util/collect");
 const fs = require("fs");
+const expect = require("expect");
 
 describe("Parses all of the invalid IDLs to check that they blow up correctly", () => {
-  const dir = pth.join(__dirname, "invalid/idl");
-  const skip = {};
-  const idls = fs.readdirSync(dir)
-    .filter(it => (/\.w?idl$/).test(it) && !skip[it])
-    .map(it => pth.join(dir, it));
-
-  for (const idl of idls) {
-    const err = JSON.parse(fs.readFileSync(
-      pth.join(__dirname, "invalid", "json", pth.basename(idl).replace(/\.w?idl/, ".json")),
-      "utf8"
-    ));
-
-    it(`should produce the right error for ${idl}`, () => {
-      let error;
-      try {
-        const ast = wp.parse(fs.readFileSync(idl, "utf8"));
-        console.log(JSON.stringify(ast, null, 4));
-      }
-      catch (e) {
-        error = e;
-      }
-      finally {
-        expect(error).toBeTruthy();
-        expect(error.message).toEqual(err.message);
-        expect(error.line).toEqual(err.line);
-      }
+  for (const test of collect("invalid", { expectError: true })) {
+    it(`should produce the right error for ${test.path}`, () => {
+      const err = test.readJSON();
+      expect(test.error).toBeTruthy();
+      expect(test.error.message).toEqual(err.message);
+      expect(test.error.line).toEqual(err.line);
     });
   }
 });
