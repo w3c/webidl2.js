a74bf7f1d674c8717562c2efc9291852d8972c40
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-06-03 18:32:31 +0900

    remove whitelist

--- test/writer.js
+++ test/writer.js
@@ -6,16 +6,10 @@ const expect = require("expect");
 const path = require("path");
 
 describe("Rewrite and parses all of the IDLs to produce the same ASTs", () => {
-  const whitelist = [
-    "documentation",
-    "documentation-dos"
-  ];
   for (const test of collect("syntax")) {
     it(`should produce the same AST for ${test.path}`, () => {
       const rewritten = wp.write(test.ast);
-      if (whitelist.includes(path.basename(test.path).slice(0, -5))) {
-        expect(rewritten).toEqual(test.text.trim());
-      }
+      expect(rewritten).toEqual(test.text);
       const diff = test.diff(wp.parse(rewritten, test.opt));
       expect(diff).toBe(undefined);
     });
