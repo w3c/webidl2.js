7d08a0c8af9fcc1eec8bc9238261d8d7b5915fec
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-06-15 13:08:36 +0900

    BREAKING CHANGE: support full whitespace conservation (#185)
    * add eof type, expose writer
    
    * remove whitelist
    
    * fix test
    
    * do not count trivia before eof

--- test/writer.js
+++ test/writer.js
@@ -2,21 +2,13 @@
 
 const { collect } = require("./util/collect");
 const wp = require("../lib/webidl2");
-const writer = require("../lib/writer");
 const expect = require("expect");
-const path = require("path");
 
 describe("Rewrite and parses all of the IDLs to produce the same ASTs", () => {
-  const whitelist = [
-    "documentation",
-    "documentation-dos"
-  ];
   for (const test of collect("syntax")) {
     it(`should produce the same AST for ${test.path}`, () => {
-      const rewritten = writer.write(test.ast);
-      if (whitelist.includes(path.basename(test.path).slice(0, -5))) {
-        expect(rewritten).toEqual(test.text.trim());
-      }
+      const rewritten = wp.write(test.ast);
+      expect(rewritten).toEqual(test.text);
       const diff = test.diff(wp.parse(rewritten, test.opt));
       expect(diff).toBe(undefined);
     });
