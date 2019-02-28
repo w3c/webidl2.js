454263f5b74f9d9c53bab54c94cd1ed172fc7dc7
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-02-28 14:21:59 +0900

    move name duplication check to validator

--- test/invalid.js
+++ test/invalid.js
@@ -11,7 +11,13 @@ describe("Parses all of the invalid IDLs to check that they blow up correctly", 
   for (const test of collect("invalid", { expectError: true, raw: true })) {
     it(`should produce the right error for ${test.path}`, () => {
       const err = test.readText();
-      expect(test.error.message + "\n").toEqual(err);
+      if (test.error) {
+        expect(test.error.message + "\n").toEqual(err);
+      } else if (test.validation) {
+        expect(test.validation.join("\n") + "\n").toEqual(err);
+      } else {
+        throw new Error("This test unexpectedly had no error");
+      }
     });
   }
 });
