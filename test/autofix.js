f1dbe2de1c71a652475c2142450983314ceb2af4
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-09-24 01:30:02 +0900

    more tests

--- test/autofix.js
+++ test/autofix.js
@@ -156,5 +156,39 @@ describe("Writer template functions", () => {
       };
     `;
     expect(autofix(input4space)).toBe(output4space);
+
+    const input1space = `
+      [Exposed=Window, Constructor]
+      interface C {
+       // less indentation
+       attribute any koala;
+      };
+    `;
+    const output1space = `
+      [Exposed=Window]
+      interface C {
+       constructor();
+       // less indentation
+       attribute any koala;
+      };
+    `;
+    expect(autofix(input1space)).toBe(output1space);
+
+    const inputTab = `
+      [Exposed=Window, Constructor]
+      interface C {
+      \t// tabbed indentation
+      \tattribute any koala;
+      };
+    `;
+    const outputTab = `
+      [Exposed=Window]
+      interface C {
+      \tconstructor();
+      \t// tabbed indentation
+      \tattribute any koala;
+      };
+    `;
+    expect(autofix(inputTab)).toBe(outputTab);
   });
 });
