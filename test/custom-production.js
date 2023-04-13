436ebdbfa45a300e597445c3281697501c50b184
pyoor <pyoor@users.noreply.github.com>
2023-04-13 15:08:19 -0400

    test: merge extension test into custom-production

--- test/custom-production.js
+++ test/custom-production.js
@@ -1,7 +1,40 @@
-"use strict";
+import expect from "expect";
 
-import { expect } from "expect";
 import { parse, write } from "webidl2";
+import { Base } from "../lib/productions/base.js";
+import {
+  autoParenter,
+  type_with_extended_attributes,
+} from "../lib/productions/helpers.js";
+
+class CustomAttribute extends Base {
+  static parse(tokeniser) {
+    const start_position = tokeniser.position;
+    const tokens = {};
+    const ret = autoParenter(
+      new CustomAttribute({ source: tokeniser.source, tokens })
+    );
+    tokens.base = tokeniser.consumeIdentifier("custom");
+    if (!tokens.base) {
+      tokeniser.unconsume(start_position);
+      return;
+    }
+    ret.idlType =
+      type_with_extended_attributes(tokeniser, "attribute-type") ||
+      tokeniser.error("Attribute lacks a type");
+    tokens.name =
+      tokeniser.consumeKind("identifier") ||
+      tokeniser.error("Attribute lacks a name");
+    tokens.termination =
+      tokeniser.consume(";") ||
+      tokeniser.error("Unterminated attribute, expected `;`");
+    return ret.this;
+  }
+
+  get type() {
+    return "custom attribute";
+  }
+}
 
 describe("Writer template functions", () => {
   const customIdl = `
@@ -48,4 +81,26 @@ describe("Writer template functions", () => {
 
   const rewritten = write(result);
   expect(rewritten).toBe(customIdl);
+});
+
+describe("Parse IDLs using custom extensions", () => {
+  [
+    ["callback interface", "callbackInterface"],
+    ["dictionary", "dictionary"],
+    ["interface", "interface"],
+    ["interface mixin", "mixin"],
+    ["namespace", "namespace"],
+  ].forEach(([type, key]) => {
+    it(`Attribute on ${type}`, () => {
+      const customIdl = `${type} Foo {
+        custom long bar;
+      };`;
+      const result = parse(customIdl, {
+        concrete: true,
+        extensions: { [key]: { extMembers: [[CustomAttribute.parse]] } },
+      });
+      expect(result[0].type).toBe(type);
+      expect(result[0].members[0].type).toBe("custom attribute");
+    });
+  });
 });
