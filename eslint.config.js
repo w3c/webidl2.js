4205f8515560d88ab30a9036accf096ca9efc227
Kagami Sascha Rosylight <saschanaz@outlook.com>
2025-08-03 22:38:10 +0200

    style: add typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from "@eslint/js";
 import globals from "globals";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import importPlugin from 'eslint-plugin-import';
+import importPlugin from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
@@ -15,19 +16,29 @@ export default [
         ...globals.browser,
         ...globals.node,
         ...globals.mocha,
-      }
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     rules: {
       "eol-last": "error",
       "import/extensions": ["error", "always"],
-      'import/no-unresolved': 0, // https://github.com/import-js/eslint-plugin-import/issues/3082
+      "import/no-unresolved": 0, // https://github.com/import-js/eslint-plugin-import/issues/3082
       "no-console": "error",
-      "no-constant-condition": ["error", { "checkLoops": false }],
+      "no-constant-condition": ["error", { checkLoops: false }],
       "no-eval": "error",
       "no-implied-eval": "error",
       "no-trailing-spaces": "error",
       "prefer-const": "error",
-      "semi": "error"
+      semi: "error",
     },
   },
+  ...tseslint.configs.recommendedTypeChecked.map(config => {
+    return {
+      files: ["lib/**"],
+      ...config
+    }
+  })
 ];
